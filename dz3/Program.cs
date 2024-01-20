c54a8f1b84fa92ced1558b7f07b0a7cf8cdec9f6
Aleksey Myakinin <amykis@mail.ru>
2024-01-20 21:05:34 +0300

    Добавлена проверка на обнаружение одинаковых сумм в нескольких строках

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -7,11 +7,14 @@
 
 // Создание двумерного массива
 
-int [,] Create2DArray(int row, int col, int min, int max){
-    int [,] array = new int [row, col];
-    for (int i = 0; i < row; i++){
-        for (int j = 0; j < col; j++){
-            array [i, j] = new Random().Next(min, max + 1);
+int[,] Create2DArray(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
         }
     }
     return array;
@@ -19,9 +22,12 @@ int [,] Create2DArray(int row, int col, int min, int max){
 
 // Вывод в консоль двумерного массива
 
-void Show2DArray(int [,] array){
-    for (int i = 0; i < array.GetLength(0); i++){
-        for (int j = 0; j < array.GetLength(1); j++){
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
             Console.Write($"{array[i, j]} ");
         }
         Console.WriteLine();
@@ -31,20 +37,25 @@ void Show2DArray(int [,] array){
 
 // Нахождение построчной суммы элементовмассива
 
-int [] TempSummArray (int [,] array){
-    int [] tempSummArray = new int [array.GetLength(0)];
-    for (int i = 0; i < array.GetLength(0); i++){
-        for (int j =0; j < array.GetLength(1); j++){
+int[] TempSummArray(int[,] array)
+{
+    int[] tempSummArray = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
             tempSummArray[i] += array[i, j];
-            }
+        }
     }
     return tempSummArray;
 }
 
 // Вывод в консоль временного массива (не обязательно)
 
-void ShowArray(int [] array){
-    for (int i = 0; i < array.GetLength(0); i++){
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
         Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
@@ -52,16 +63,29 @@ void ShowArray(int [] array){
 
 // Проверка наименьшей суммы в двумерном массиве (суммы строк лежат во временном массиве)
 
-void MinSumOfElement(int [] array){
+void MinSumOfElement(int[] array)
+{
     int minIndex = 0;
     int minSumm = array[0];
+    int count = 0;
     for (int i = 0; i < array.Length; i++){
-        if (minSumm > array[i]){
+        if (array[i] < minSumm){
+            minSumm = array[i];
             minIndex = i;
         }
     }
-    Console.WriteLine();
-    Console.Write($"Минимальная сумма элементов в строке с индексом {minIndex}");
+    for (int j = 0; j < array.Length; j++){
+        if (array[minIndex] == array[j]){
+            count += 1;
+        }
+    }
+    if (count > 1){
+        Console.WriteLine();
+        Console.Write($"Минимальная сумма элементов встречается в нескольких строках и равна {array[minIndex]}");
+    } else {
+        Console.WriteLine();
+        Console.Write($"Минимальная сумма элементов в строке с индексом {minIndex}");
+    }
 }
 
 Console.Clear();
@@ -74,11 +98,11 @@ int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите максимальный элемент: ");
 int max = Convert.ToInt32(Console.ReadLine());
 
-int [,] array = Create2DArray(row, col, min, max);
+int[,] array = Create2DArray(row, col, min, max);
 
 Show2DArray(array);
 
-int [] tempSummArray = TempSummArray(array);
+int[] tempSummArray = TempSummArray(array);
 
 ShowArray(tempSummArray);
 
